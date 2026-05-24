977f0985eea1c2208f2f276cbf90183474283934
Michael-John Uren <michael16.uren@gmail.com>
2026-05-24 18:26:36 +0200

    Update project paths and versioning in configuration files for CyberSecurityBot

--- CyberSecurityBot/obj/Debug/net10.0-windows/CyberSecurityBot.AssemblyInfo.cs
+++ CyberSecurityBot/obj/Debug/net10.0-windows/CyberSecurityBot.AssemblyInfo.cs
@@ -13,7 +13,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("CyberSecurityBot")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+9ef4917a026e41358c966412a44f8b9f6a3687bb")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+35e243d9dd9c85734ee12f49116788902eb65b0a")]
 [assembly: System.Reflection.AssemblyProductAttribute("CyberSecurityBot")]
 [assembly: System.Reflection.AssemblyTitleAttribute("CyberSecurityBot")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
